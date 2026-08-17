768f2d6eb1be58af1d6b4b332da330a50b20a9dd
Fahadazeem-ui <ziddibusiness@gmail.com>
2026-08-17 22:31:47 +0500

    Update PluginBootstrap.kt

--- src/main/kotlin/io/github/mindzard/mythicinvasion/bootstrap/PluginBootstrap.kt
+++ src/main/kotlin/io/github/mindzard/mythicinvasion/bootstrap/PluginBootstrap.kt
@@ -1,8 +1,11 @@
 package io.github.mindzard.mythicinvasion.bootstrap
 
 import io.github.mindzard.mythicinvasion.MythicInvasionPlugin
+import io.github.mindzard.mythicinvasion.application.ecosystem.EcosystemCoordinator
+import io.github.mindzard.mythicinvasion.application.ecosystem.EcosystemEngine
 import io.github.mindzard.mythicinvasion.concurrency.CoroutineEngine
 import io.github.mindzard.mythicinvasion.config.ConfigurationManager
+import io.github.mindzard.mythicinvasion.infrastructure.paper.ecosystem.PlayerSnapshotCollector
 
 class PluginBootstrap(
     private val plugin: MythicInvasionPlugin
@@ -27,12 +30,53 @@ class PluginBootstrap(
             coroutineEngine
         )
 
+        val playerSnapshotCollector =
+            PlayerSnapshotCollector(plugin)
+
+        registry.registerPlayerSnapshotCollector(
+            playerSnapshotCollector
+        )
+
+        val ecosystemEngine =
+            EcosystemEngine(plugin)
+
+        registry.registerEcosystemEngine(
+            ecosystemEngine
+        )
+
+        val ecosystemCoordinator =
+            EcosystemCoordinator(
+                plugin = plugin,
+                coroutineEngine = coroutineEngine,
+                snapshotCollector = playerSnapshotCollector,
+                ecosystemEngine = ecosystemEngine,
+                updateIntervalMillisProvider = {
+                    configurationManager
+                        .ecosystemUpdateIntervalTicks()
+                        .times(50L)
+                }
+            )
+
+        registry.registerEcosystemCoordinator(
+            ecosystemCoordinator
+        )
+
+        ecosystemCoordinator.start()
+
         plugin.logger.info(
             "Configuration system initialized."
         )
 
         plugin.logger.info(
             "Coroutine engine initialized."
+        )
+
+        plugin.logger.info(
+            "Player snapshot collector initialized."
+        )
+
+        plugin.logger.info(
+            "Ecosystem engine initialized."
         )
 
         return registry
