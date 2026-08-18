007458c5ff89b538a859bd349c98f44476a10309
Fahadazeem-ui <ziddibusiness@gmail.com>
2026-08-18 07:21:37 +0500

    Update ServiceRegistry.kt

--- src/main/kotlin/io/github/mindzard/mythicinvasion/bootstrap/ServiceRegistry.kt
+++ src/main/kotlin/io/github/mindzard/mythicinvasion/bootstrap/ServiceRegistry.kt
@@ -1,5 +1,8 @@
 package io.github.mindzard.mythicinvasion.bootstrap
 
+import io.github.mindzard.mythicinvasion.application.behaviour.BehaviourEventBuffer
+import io.github.mindzard.mythicinvasion.application.behaviour.BehaviourProcessor
+import io.github.mindzard.mythicinvasion.application.behaviour.BehaviourProfileStore
 import io.github.mindzard.mythicinvasion.application.ecosystem.EcosystemCoordinator
 import io.github.mindzard.mythicinvasion.application.ecosystem.EcosystemEngine
 import io.github.mindzard.mythicinvasion.concurrency.CoroutineEngine
@@ -21,6 +24,15 @@ class ServiceRegistry {
         private set
 
     lateinit var ecosystemCoordinator: EcosystemCoordinator
+        private set
+
+    lateinit var behaviourEventBuffer: BehaviourEventBuffer
+        private set
+
+    lateinit var behaviourProfileStore: BehaviourProfileStore
+        private set
+
+    lateinit var behaviourProcessor: BehaviourProcessor
         private set
 
     fun registerConfigurationManager(
@@ -51,5 +63,23 @@ class ServiceRegistry {
         coordinator: EcosystemCoordinator
     ) {
         ecosystemCoordinator = coordinator
+    }
+
+    fun registerBehaviourEventBuffer(
+        buffer: BehaviourEventBuffer
+    ) {
+        behaviourEventBuffer = buffer
+    }
+
+    fun registerBehaviourProfileStore(
+        store: BehaviourProfileStore
+    ) {
+        behaviourProfileStore = store
+    }
+
+    fun registerBehaviourProcessor(
+        processor: BehaviourProcessor
+    ) {
+        behaviourProcessor = processor
     }
 }
