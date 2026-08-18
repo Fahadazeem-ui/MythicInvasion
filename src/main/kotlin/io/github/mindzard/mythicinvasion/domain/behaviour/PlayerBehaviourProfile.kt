968a06c787b910b6d601ff169cfe5f94e0ce2d8e
Fahadazeem-ui <ziddibusiness@gmail.com>
2026-08-18 07:20:13 +0500

    Create PlayerBehaviourProfile.kt

---
+++ src/main/kotlin/io/github/mindzard/mythicinvasion/domain/behaviour/PlayerBehaviourProfile.kt
@@ -0,0 +1,15 @@
+package io.github.mindzard.mythicinvasion.domain.behaviour
+
+import java.util.UUID
+
+data class PlayerBehaviourProfile(
+    val playerId: UUID,
+    val totalEvents: Long = 0L,
+    val joins: Long = 0L,
+    val quits: Long = 0L,
+    val movements: Long = 0L,
+    val blocksBroken: Long = 0L,
+    val blocksPlaced: Long = 0L,
+    val combatActions: Long = 0L,
+    val lastSeenMillis: Long = 0L
+)
