99c794064b606204197b1f5cdba4e6cd19725635
Fahadazeem-ui <ziddibusiness@gmail.com>
2026-08-18 14:16:54 +0500

    Create StrategyAction.kt

---
+++ src/main/kotlin/io/github/mindzard/mythicinvasion/domain/ai/StrategyAction.kt
@@ -0,0 +1,13 @@
+package io.github.mindzard.mythicinvasion.domain.ai
+
+enum class StrategyAction {
+    NONE,
+
+    INCREASE_HOSTILE_PRESSURE,
+
+    FOCUS_HIGH_PRESSURE_PLAYERS,
+
+    DEFEND_SETTLEMENTS,
+
+    SCOUT_SETTLEMENTS
+}
